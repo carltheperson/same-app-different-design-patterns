f6349efe54168b554076bf483e95f831fdc2ee51
Carl Riis <carltheperson@protonmail.com>
2021-09-12 12:05:48 +0200

    Registering event listeners

--- event-driven-architecture/src/index.ts
+++ event-driven-architecture/src/index.ts
@@ -2,6 +2,9 @@ import { connectToMongo } from "./persistence/mongo";
 import { startServer } from "./presentation/api";
 import { servePage } from "./presentation/page";
 
+import "./voting-process/process";
+import "./ranking-process/process";
+
 connectToMongo();
 startServer();
 servePage();
